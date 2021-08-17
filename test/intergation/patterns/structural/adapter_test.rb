1ea9445f32fc047cef6d5207f7fcc6a8fb8de014
Vitaly Stukov <vstukov@beltechsoftware.com>
2021-08-17 11:54:40 +0300

    Improved structure of catalogs.

--- test/intergation/patterns/structural/adapter_test.rb
+++ test/intergation/patterns/structural/adapter_test.rb
@@ -2,7 +2,7 @@ require 'unit/test_helper'
 
 class AdapterTest < MiniTest::Test
   def test_text_shape
-    text_shape = Patterns::Structural::Adapter::TextShape.new(DrawingEditor::TextView.new)
+    text_shape = DrawingEditor::Adapter::TextShape.new(DrawingEditor::TextView.new)
 
     assert_equal(5, text_shape.bounding_box(1, 2))
     refute(text_shape.is_empty)
