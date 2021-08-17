a35321fba012a65dc894d941d8c1cc008e89164f
Vitaly Stukov <vstukov@beltechsoftware.com>
2021-08-17 12:07:44 +0300

    Improved structure of catalogs.

---
+++ lib/compiler/facade/compiler_facade.rb
@@ -0,0 +1,20 @@
+module Compiler
+  module Facade
+    class CompilerFacade
+      def compile(input, output)
+        scanner = Compiler::Scanner.new(input)
+        builder = Compiler::ProgramNodeBuilder.new(output)
+        parser = Compiler::Parser.new
+
+        parser.parse(scanner, builder)
+
+        generator = Compiler::Generators::RiscCodeGenerator.new(output)
+
+        parse_tree = builder.get_root_node
+        parse_tree.traverse(generator)
+
+        parse_tree
+      end
+    end
+  end
+end
--- test/intergation/patterns/structural/facade_test.rb
+++ test/intergation/patterns/structural/facade_test.rb
@@ -2,13 +2,13 @@ require 'unit/test_helper'
 
 class FacadeTest < MiniTest::Test
   def test_expression_node
-    compiler = Patterns::Structural::Facade::CompilerFacade.new
+    compiler = Compiler::Facade::CompilerFacade.new
 
     assert_instance_of(Compiler::Nodes::ExpressionNode, compiler.compile('1', 'code'))
   end
 
   def test_statement_node
-    compiler = Patterns::Structural::Facade::CompilerFacade.new
+    compiler = Compiler::Facade::CompilerFacade.new
 
     assert_instance_of(Compiler::Nodes::StatementNode, compiler.compile('1', 1))
   end
